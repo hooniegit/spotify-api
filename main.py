32a7d28cdaf4f028863caf72c16e959384ec481e
hooniegit <neivekim76@gmail.com>
2023-11-13 15:38:01 +0900

    231113 fix: hdfs 라우터 삭제

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from router import load_datas, load_hdfs
 
 app = FastAPI()
 app.include_router(load_datas.router)
-app.include_router(load_hdfs.router)
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=4000)
